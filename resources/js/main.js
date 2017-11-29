8c9fd14685bc056cff64c50609fabe03dd8d4fc4
harjindergill\gill <laddivaliant@live.com>
2017-11-29 11:53:34 -0800

    navigation bar is working for all screens

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,10 @@
 $('document').ready(function(){
-	
+	// naviagtion bar appear on small screen on button clicked
+	$('#navButton').click(function(){
+		if($('#menuList').hasClass("collapse")){
+			$('#menuList').removeClass("collapse");
+		}else{
+			$('#menuList').addClass("collapse");
+		}
+	});
 });
